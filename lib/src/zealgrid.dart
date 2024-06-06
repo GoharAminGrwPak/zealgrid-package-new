d12c0ba5f10a036d20fcea259e6124092f55521d
Muhammad Abubakar <manipoint@gmail.com>
2024-06-06 13:17:40 +0500

    implement . notation
    implement . notation

--- lib/src/zealgrid.dart
+++ lib/src/zealgrid.dart
@@ -1,3 +1,5 @@
+import 'dart:nativewrappers/_internal/vm/lib/mirrors_patch.dart';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 
@@ -9,39 +11,46 @@ class Zealgrid {
 
   static final DatabaseReference _database = FirebaseDatabase.instance.ref();
 
-  static Zealgrid getInstance({String path = ''}) {
-    return Zealgrid._(path: path);
-  }
-
-  Zealgrid operator [](String key) {
-    return Zealgrid._(path: '$path/$key');
+  static Future<Zealgrid> getInstance({String path = ''}) async {
+    Zealgrid instance = Zealgrid._(path: path);
+    await instance._fetchData();
+    return instance;
   }
 
   Future<void> _fetchData() async {
-    if (_data.isEmpty) {
-      try {
-        DataSnapshot snapshot = await _database.child(path).get();
-        if (snapshot.value != null && snapshot.value is Map) {
-          _data.addAll(Map<String, dynamic>.from(snapshot.value as Map));
-        }
-      } catch (e) {
-        if (kDebugMode) {
-          print('Error fetching data: $e');
-        }
+    try {
+      DataSnapshot snapshot = await _database.child(path).get();
+      if (snapshot.value != null && snapshot.value is Map) {
+        _data.addAll(Map<String, dynamic>.from(snapshot.value as Map));
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error fetching data: $e');
       }
     }
   }
 
-  Future<T?> _getValue<T>(String key) async {
-    await _fetchData();
-    return _data[key] as T?;
+  dynamic _getProperty(String propertyName) {
+    List<String> properties = propertyName.split('.');
+    dynamic currentData = _data;
+
+    for (String property in properties) {
+      if (currentData is Map<String, dynamic> && currentData.containsKey(property)) {
+        currentData = currentData[property];
+      } else {
+        throw Exception('Property not found: $propertyName');
+      }
+    }
+
+    return currentData;
   }
 
-  Future<String?> getString(String key) async => await _getValue<String>(key);
-  Future<int?> getInt(String key) async => await _getValue<int>(key);
-  Future<bool?> getBool(String key) async => await _getValue<bool>(key);
-  Future<Map<String, dynamic>?> getObject(String key) async => await _getValue<Map<String, dynamic>>(key);
-  Future<List<dynamic>?> getList(String key) async => await _getValue<List<dynamic>>(key);
-
-  Future<dynamic> getValue(String key) async => await _getValue(key);
+  @override
+  noSuchMethod(Invocation invocation) {
+    if (invocation.isGetter) {
+      final propertyName = MirrorSystem.getName(invocation.memberName);
+      return _getProperty(propertyName);
+    }
+    return super.noSuchMethod(invocation);
+  }
 }
